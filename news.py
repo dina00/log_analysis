7e933475567ce7e338594279a195f2ed561a03c2
Dina <es-dina.abdullah1415@alexu.edu.eg>
2019-12-05 13:52:26 +0200

    modified news.py

--- news.py
+++ news.py
@@ -1,47 +1,93 @@
+#!/usr/bin/env python3
 import psycopg2
 
+
 def q1():
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  c.execute("select articles.title , count(*) as number_of_views from articles,log where log.path='/article/' || articles.slug group by articles.title order by number_of_views desc limit 3")
-  posts = c.fetchall()
-  db.close()
-  return posts
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute('''SELECT articles.title,
+       count(*) AS number_of_views
+FROM articles,
+     log
+WHERE log.path='/article/' || articles.slug
+GROUP BY articles.title
+ORDER BY number_of_views DESC
+LIMIT 3''')
+    posts = c.fetchall()
+    db.close()
+    return posts
+
 
 def q2():
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  c.execute("select authors.name , count(log.path) as views from authors,log, articles where log.path='/article/' || articles.slug and authors.id=articles.author group by authors.name order by views desc;")
-  posts = c.fetchall()
-  db.close()
-  return posts
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+    c.execute('''SELECT authors.name,
+       count(log.path) AS VIEWS
+FROM authors,
+     log,
+     articles
+WHERE log.path='/article/' || articles.slug
+  AND authors.id=articles.author
+GROUP BY authors.name
+ORDER BY VIEWS DESC;''')
+    posts = c.fetchall()
+    db.close()
+    return posts
+
 
 def q3():
-  db = psycopg2.connect("dbname=news")
-  c = db.cursor()
-  #create view up as select date(log.time)as date,count(status) as cnt from log where status like '404 NOT FOUND' group by date
-  #create view down as select date(log.time) as date,count(status) as cnt from log group by date
-  #create view error as select down.date ,CAST(up.cnt as FLOAT)*100/down.cnt as errorperc from up,down where up.date=down.date order by errorperc")
-  c.execute("select * from error where errorperc>1")
+    db = psycopg2.connect("dbname=news")
+    c = db.cursor()
+#     # CREATE VIEW up AS
+# SELECT date(log.time)AS date,
+#        count(status) AS cnt
+# FROM log
+# WHERE status LIKE '404 NOT FOUND'
+# GROUP BY date
+#     # CREATE VIEW down AS
+# SELECT date(log.time) AS date,
+#        count(status) AS cnt
+# FROM log
+# GROUP BY date
+#     # CREATE VIEW error AS
+# SELECT down.date,
+#        CAST(up.cnt AS FLOAT)*100/down.cnt AS errorperc
+# FROM up,
+#      down
+# WHERE up.date=down.date
+# ORDER BY errorperc
+    c.execute('''SELECT *
+FROM error
+WHERE errorperc > 1''')
+    posts = c.fetchall()
+    db.close()
+    return posts
 
-  posts = c.fetchall()
-  db.close()
-  return posts
+
+if __name__ == '__main__':
+
+    # this is to prevent executing
+    # the code when imported, ignore.
+    answer1 = q1()
+    print('Q1: What are the most popular three articles of all time?')
+    for article in enumerate(answer1):
+        print('name: %s - views %d' % (article[1][0], article[1][1]))
+    print('\n')
+
+    answer2 = q2()
+    print('Q2: Who are the most popular authors of all time?')
+    for author in enumerate(answer2):
+        print('name: %s - views %d' % (author[1][0], author[1][1]))
+    print('\n')
+
+    answer3 = q3()
+    print('Q3: Which days have errors exceeding 1%?')
+    for error in enumerate(answer3):
+        print('day: %s - error percentage %f' % (error[1][0], error[1][1]))
+    print('\n')
+
+    print('The End')
 
 
-answer1=q1()
-print('Q1: What are the most popular three articles of all time?')
-for article in enumerate(answer1):
-  print('name: %s - views %d' % (article[1][0], article[1][1]))
-
-answer2=q2()
-print('Q2: Who are the most popular authors of all time?')
-for author in enumerate(answer2):
-  print('name: %s - views %d' % (author[1][0], author[1][1]))
-
-answer3=q3()
-print('Q3: Which days have errors exceeding 1%?')
-for error in enumerate(answer3):
-  print('day: %s - error percentage %f' % (error[1][0], error[1][1]))
-
-print('The End')
+else:
+    print('importing...')
